c8aff6cb5d68e4c12e5ad7f6805b21f29c0b3ab4
2infinity-gh <48837503+2infinity-gh@users.noreply.github.com>
2019-07-18 09:14:00 +0200

    Update Paperwallet print

--- js/payboxx.js
+++ js/payboxx.js
@@ -67,6 +67,12 @@ setCurrentTab(coinID);
 
 ////////////////  BZX Paperwallet ////////////////////
 
+	var mobilecheck = function() {
+	  var check = false;
+	  (function(a){if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino|android|ipad|playbook|silk/i.test(a)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0,4))) check = true;})(navigator.userAgent||navigator.vendor||window.opera);
+	  return check;
+	};
+
 	$("#newPaperwalletBtnbzx").click(function(){
 		if($("#newAddressbzx").val()==""){
 			$("#newKeysBtnbzx").click();
@@ -87,13 +93,15 @@ setCurrentTab(coinID);
 		'Bitcoinzero wallet and importing your private key to the wallet. Keep in mind when you import your single key to a bitcoinzero p2p client and spend funds your key will be '+
 		'bundled with other private keys in the p2p client wallet. When you perform a transaction your change will be sent to another bitcoin zero address within the p2p client wallet. You must then '+
 		'backup the p2p client wallet and keep it safe as your remaining coins will be stored there. Satoshi advised that one should never delete a wallet.</div>');
-		paperwallet.document.close();
+		//paperwallet.document.close();
 		paperwallet.focus();
 		new QRCode(paperwallet.document.getElementById("qraddressbzx"), {text: $("#newAddressbzx").val(), width: 200, height: 200});
 		new QRCode(paperwallet.document.getElementById("qrpubkeybzx"), {text: $("#newPubKeybzx").val(), width: 200, height: 200});
 		new QRCode(paperwallet.document.getElementById("qrprivkeybzx"), {text: $("#newPrivKeybzx").val(), width: 200, height: 200});
 		paperwallet.print();
+		if(!mobilecheck()){
 		paperwallet.close();
+		}
 	});
 	
 ///////////////// GXX Paperwallet ///////////////////	
@@ -117,13 +125,15 @@ setCurrentTab(coinID);
 		'Gravitycoin wallet and importing your private key to the wallet. Keep in mind when you import your single key to a Gravitycoin p2p client and spend funds your key will be '+
 		'bundled with other private keys in the p2p client wallet. When you perform a transaction your change will be sent to another GravityCoin address within the p2p client wallet. You must then '+
 		'backup the p2p client wallet and keep it safe as your remaining coins will be stored there. Satoshi advised that one should never delete a wallet.</div>');		
-		paperwallet.document.close();
+		//paperwallet.document.close();
 		paperwallet.focus();
 		new QRCode(paperwallet.document.getElementById("qraddressgxx"), {text: $("#newAddressgxx").val(), width: 200, height: 200});
 		new QRCode(paperwallet.document.getElementById("qrpubkeygxx"), {text: $("#newPubKeygxx").val(), width: 200, height: 200});
 		new QRCode(paperwallet.document.getElementById("qrprivkeygxx"), {text: $("#newPrivKeygxx").val(), width: 200, height: 200});
 		paperwallet.print();
+		if(!mobilecheck()){
 		paperwallet.close();
+		}
 	});
 	
 ///////////////////// BCZ Paperwallet ////////////////	
@@ -147,13 +157,15 @@ setCurrentTab(coinID);
 		'Bitcoin CZ wallet and importing your private key to the wallet. Keep in mind when you import your single key to a bitcoin CZ p2p client and spend funds your key will be '+
 		'bundled with other private keys in the p2p client wallet. When you perform a transaction your change will be sent to another Bitcoin CZ address within the p2p client wallet. You must then '+
 		'backup the p2p client wallet and keep it safe as your remaining coins will be stored there. Satoshi advised that one should never delete a wallet.</div>');
-		paperwallet.document.close();
+		//paperwallet.document.close();
 		paperwallet.focus();
 		new QRCode(paperwallet.document.getElementById("qraddressbcz"), {text: $("#newAddressbcz").val(), width: 200, height: 200});
 		new QRCode(paperwallet.document.getElementById("qrpubkeybcz"), {text: $("#newPubKeybcz").val(), width: 200, height: 200});
 		new QRCode(paperwallet.document.getElementById("qrprivkeybcz"), {text: $("#newPrivKeybcz").val(), width: 200, height: 200});
 		paperwallet.print();
+		if(!mobilecheck()){
 		paperwallet.close();
+		}
 	});
 
 
